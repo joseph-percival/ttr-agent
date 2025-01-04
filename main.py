a71eae04fc548476492715c7561bbc331e5d40d6
Joseph Percival <joseph.percival-2@student.manchester.ac.uk>
2025-01-04 20:33:13 +0000

    add command line arguments

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from src.Board import Board
-from src.Player import Player
+from src.TicketToRideGame import TicketToRideGame
 
 import sys
 
+# usage:
+# python3 main.py
+# (computer players only)
+
+# python3 main.py <HUMAN_COLOUR> <HUMAN_COLOUR> [...]
+# add human players 
+# allowed colours: RED BLUE GREEN YELLOW BLACK
 if __name__ == "__main__":
+    print(sys.argv)
+    playerColours = (
+        "RED",
+        "BLUE",
+        "GREEN",
+        "YELLOW",
+        "BLACK"
+    )
+    assert len(sys.argv) <= 6 # TOO MANY ARGUMENTS
+    if len(sys.argv) > 1:
+        for item in sys.argv[1:]:
+            assert(item in playerColours) # PLAYER COLOUR NOT RECOGNISED
     print("setting up game")
 
-    board = Board()
-    print(sys.argv)
+    gameLoop = TicketToRideGame()
