cf54883873c238eb4691298fe5a18bd5e48873dc
Joseph Percival <joseph.percival-2@student.manchester.ac.uk>
2025-01-04 23:36:36 +0000

    add discard routes method

--- src/Player.py
+++ src/Player.py
@@ -1,13 +1,24 @@
-from src.Enums import PlayerType, PlayerColour
+from src.Enums import PlayerType, PlayerColour, CardType
 from src.CardPile import CardPile
 
 class Player:
     def __init__(self, colour:PlayerColour, 
-                 startTrains:CardPile,
-                 startRoutes:CardPile,
                  playerType:PlayerType=PlayerType.COMPUTER):
         self.colour=colour
         self.playerType=playerType
-        self.trainCards=startTrains
-        self.startRoutes=startRoutes
+        self.trainCards=CardPile()
+        self.routeCards=CardPile()
         self.score=0
+        self.numTrains=45
+
+    def discardRoutes(self):
+        print(f"Discard any of these routes, but keep at least one:")
+        for i, card in enumerate(self.routeCards):
+            if card.cardType is not CardType.LONG_ROUTE:
+                print(f"{i}: {card.vertices[0].name} to {card.vertices[1].name}: {card.cardPoints} points")
+            else:
+                print(f"{i}: {card.vertices[0].name} to {card.vertices[1].name}: {card.cardPoints} points [LONG ROUTE - CANNOT DISCARD]")
+        input()
+
+    def playTurn(self):
+        pass
