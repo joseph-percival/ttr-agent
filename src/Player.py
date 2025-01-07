33c86a99db4656790d28028d2f3837506c822cde
Joseph Percival <joseph.percival-2@student.manchester.ac.uk>
2025-01-07 20:52:21 +0000

    add choose route option

--- src/Player.py
+++ src/Player.py
@@ -10,15 +10,33 @@ class Player:
         self.routeCards=CardPile()
         self.score=0
         self.numTrains=45
+        self.numStations=3
 
-    def discardRoutes(self):
-        print(f"Discard any of these routes, but keep at least one:")
-        for i, card in enumerate(self.routeCards):
-            if card.cardType is not CardType.LONG_ROUTE:
-                print(f"{i}: {card.vertices[0].name} to {card.vertices[1].name}: {card.cardPoints} points")
-            else:
-                print(f"{i}: {card.vertices[0].name} to {card.vertices[1].name}: {card.cardPoints} points [LONG ROUTE - CANNOT DISCARD]")
-        input()
+    def printRoutes(self, cards:CardPile=None):
+        cards = self.routeCards if cards == None else cards
+        for i, card in enumerate(cards):
+            match card.cardType:
+                case CardType.LONG_ROUTE:
+                    print(f"{i}: {card.vertices[0].name} to {card.vertices[1].name}: {card.cardPoints} points [LONG ROUTE]")
+                case _:
+                    print(f"{i}: {card.vertices[0].name} to {card.vertices[1].name}: {card.cardPoints} points")
+                
 
-    def playTurn(self):
-        pass
+    def chooseRoutes(self, newRoutes:CardPile):
+        print(f"Here are your current routes:")
+        self.printRoutes()
+        choice = None
+
+        while choice != "" and len(newRoutes) > 1:
+            print(f"Discard any of these routes, but keep at least one:")
+            self.printRoutes(cards=newRoutes)
+            print("Type the number of a route you wish to discard, or press enter to skip")
+            choice = input(": ")
+            match choice:
+                case str() if choice.isdigit() and int(choice) < len(newRoutes):
+                    del newRoutes[int(choice)]
+                case "":
+                    break
+                case _:
+                    print("Invalid choice, please enter a valid route number.")
+        self.routeCards.extend(newRoutes)
