5c382b4805650ab79b18a147e8112b5a844b3039
Joseph Percival <joseph.percival-2@student.manchester.ac.uk>
2025-01-07 20:52:43 +0000

    add playTurn loop for players

--- src/TicketToRideGame.py
+++ src/TicketToRideGame.py
@@ -25,11 +25,15 @@ class TicketToRideGame:
         # distribute starting cards
         for player in self.players:
             player.routeCards.append(self.board.longRoutePile.pop())
-            for _ in range(3):
-                player.routeCards.append(self.board.routePile.pop())
+            newRouteCards = []
             for _ in range(4):
                 player.trainCards.append(self.board.trainPile.pop())
-            player.discardRoutes()
+            for _ in range(3):
+                newRouteCards.append(self.board.routePile.pop())
+
+            print(f"-- {player.colour.name} --")
+            player.chooseRoutes(newRouteCards)
+
         for _ in range(5):
             self.board.trainPool.append(self.board.trainPile.pop())
 
@@ -39,7 +43,7 @@ class TicketToRideGame:
             current = self.players[0]
             for connection in self.board.connections:
                 print(f"{connection.occupiedBy}: {connection.vertices[0].name} to {connection.vertices[1].name}: {connection.length}, {connection.colour}, {connection.connectionType.name}")
-            current.playTurn()
+            self.playTurn(current)
             # turn of play rotates
             self.players.rotate(-1)
             self.lowestTrains = min(self.lowestTrains, current.numTrains)
@@ -47,6 +51,30 @@ class TicketToRideGame:
 
         # everyone from 0 to end of deque -1 gets another turn
         for player in self.players[:-1]:
-            player.playTurn()
+            self.playTurn(player)
 
+    def playTurn(self, player:Player):
+        print(f"{player.colour}'s turn: choose an action")
+        choice = ""
+        while choice == "":
+            choice = input()
+            match choice:
+                case "1":
+                    # draw train cards
+                    pass
+                case "2":
+                    # play a route
+                    pass
+                case "3":
+                    # place a station
+                    pass
+                case "4":
+                    # draw route cards
+                    newRouteCards = []
+                    for _ in range(3):
+                        newRouteCards.append(self.board.routePile.pop())
+                    player.chooseRoutes(newRouteCards)
+                case _:
+                    choice = ""
+                    print("invalid choice")
         
